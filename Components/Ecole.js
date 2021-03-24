65bf26df7b737b36d7683ede90dedbdb917dfcf9
Pas921 <adrien.popo60@gmail.com>
2021-03-24 12:51:07 +0100

    modifs24/03

--- Components/Ecole.js
+++ Components/Ecole.js
@@ -15,9 +15,11 @@ class Ecole extends React.Component {
                 <Text> { ecoles[4].field1 } </Text>
                 <Text> { ecoles[4]['ECHANGES INTERNATIONAUX'] } </Text>
                 {/*<Image source={{uri: 'https://lesfacultes.univ-catholille.fr/sites/default/files/styles/1094x461/public/panoramique_catho_0.jpg?itok=7vJS39ei'}}></Image>*/}
+                <Text onPress={() => Linking.openURL( { /* importer le lien de l'ecole */ } )}> Lien école </Text>
                 <Text> Niveau de langue demandé : { ecoles[4]['Niveau langues demand� / Acad�mique'] } </Text>
                 <Text> Nombre de semestres disponibles : { ecoles[4]['Semestres dispo pour d�part 21-22'] } </Text>
                 <FiliereDispo/>
+                <Text> Calendrier academique : { /* importer le calendrier academique */ } </Text>
             </View>
 
         )
@@ -26,4 +28,3 @@ class Ecole extends React.Component {
 }
 
 export default Ecole
-
--- Components/Essai.js
+++ Components/Essai.js
@@ -3,25 +3,72 @@ import { View, Text, FlatList } from 'react-native'
 
 import * as liste from "../Helpers/liste.json"
 
-const pays = []
-var existe = 0
+const pays = [];
+const paystrie = [];
+const ecoles = [];
+var existe = 0;
 
 const listePays = () => {
+
     for ( var i in liste ) {
         for ( var j in pays ) {
-            if (liste[i].field1 == pays[j].key ) {
-                existe = 1
+
+            if ( liste[i].field1 == pays[j]
+                || liste[i].field1 == "Europe - Erasmus" 
+                || liste[i].field1 == "Europe - Hors Erasmus - MIC ou conventions globales" 
+                || liste[i].field1 == "Europe - Hors Erasmus" 
+                || liste[i].field1 == "Hors Europe - Bilat�rales et Conventions globales ou ICL"
+                || liste[i].field1 == "Total disponible"
+                || liste[i].field1 == "" ) {
+
+                existe = 1 ;
+
             }
         }
-        if (existe == 0) {
-            pays.push({ key: liste[i].field1 })
+
+        if ( existe == 0 ) {
+
+            pays.push(liste[i].field1)
+
         }
-        existe = 0
+        existe = 0 ;
     }
 } 
 
-listePays()
-console.log(pays)
+const trisPays = () => {
+
+    listePays();
+
+    for ( var i in pays ) {
+
+        paystrie.push({ key: pays.sort()[i] }) ;
+
+    }
+
+}
+
+const listeEcole = ( monPays ) => {
+
+    var temp = [];
+
+    for ( var i in liste ) {
+        if ( liste[i].field1 == monPays ) {
+            temp.push(liste[i]['ECHANGES INTERNATIONAUX']) ;
+        }
+    }
+
+    for ( var j in temp) {
+        
+        ecoles.push({ key: temp.sort()[j] })
+
+    }
+
+    return(ecoles);
+
+}
+
+
+trisPays()
 
 class Essai extends React.Component {
 
@@ -31,7 +78,7 @@ class Essai extends React.Component {
 
             <View>
                 <FlatList
-                    data = { pays }
+                    data = { paystrie }
                     renderItem = { ( { item } ) => <Text> { item.key } </Text> }
                 />
             </View>
